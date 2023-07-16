9847f6b64332ea0f0d72c98e62bdbeb0ac39e59b
temka778 <113933105+temka778@users.noreply.github.com>
2023-07-16 14:40:16 +0400

    После Сочи

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=120)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
+    description = models.TextField('Описание')
+    completed = models.BooleanField('Выполнено', default=False)
 
     def _str_(self):
         return self.title
